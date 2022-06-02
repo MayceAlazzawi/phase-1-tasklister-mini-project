811c3563a15924dd0115e5e5de7721578d1980ee
MayceAlazzawi <maynab4@gmail.com>
2022-06-02 22:05:18 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+const tasks = document.getElementById('tasks');
+const form = document.getElementById('create-task-form');
+const input = document.getElementById('new-task-description');
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+ const add = (e) => {
+    e.preventDefault();
+    const li = document.createElement('li');
+    const remove = document.createElement('button');
+    remove.textContent="X"
+  
+    li.textContent = input.value;
+    li.appendChild(remove)
+    tasks.append(li)
+    remove.addEventListener("click", function(e){
+      this.parentNode.parentNode.remove();
+  })}
+  form.addEventListener('submit', add)
 });
